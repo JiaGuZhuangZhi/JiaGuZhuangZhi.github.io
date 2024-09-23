54606ddf2e1e1d5240f5c360ed5c77c331b9aa37
JiaGuZhuangZhi <2280718448@qq.com>
2024-09-23 21:54:03 +0800

    Site updated: 2024-09-23 21:54:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/08/24080801/"];function toRandomPost(){
+var posts=["2024/09/23/24092301/","2024/08/08/24080801/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
