c1b1b7a2d7b0dcec7d5d350469afded14f5e2f80
JiaGuZhuangZhi <2280718448@qq.com>
2024-12-31 12:17:12 +0800

    Site updated: 2024-12-31 12:17:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/08/24080801/","2024/09/23/24092301/","2024/12/26/24122601/"];function toRandomPost(){
+var posts=["2024/08/08/24080801/","2024/09/23/24092301/","2024/12/30/24123001/","2024/12/26/24122601/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
